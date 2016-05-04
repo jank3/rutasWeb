897b07a67f7a32b496afb9fd755bbe18a6403ca8
Pablo Corrado <corrado.pablom@gmail.com>
2016-05-03 22:27:06 -0300

    Primer commit. Seguir viendo json to matrix

---
+++ RutasWeb/RutasWeb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RutasWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ruta(HttpPostedFileBase file)
+        {
+            string path = Server.MapPath("~/Files/" + file.FileName);
+            file.SaveAs(path);
+            ViewBag.Path = path;
+
+            string json = string.Empty;
+            using (StreamReader r = new StreamReader(path))
+            {
+                json = r.ReadToEnd();
+
+                dynamic array = JsonConvert.DeserializeObject(json);
+            }
+
+            return View();
+        }
+
+    }
+}
